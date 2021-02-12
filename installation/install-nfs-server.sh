e8de4fb0849462d4617272adeed981e42420000c
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2021-02-12 16:58:43 +0700

    add some config

---
+++ installation/install-nfs-server.sh
@@ -0,0 +1,9 @@
+sudo yum -y install nfs-utils libnfsidmap
+sudo systemctl enable rpcbind
+sudo systemctl enable nfs-server
+sudo systemctl enable nfs-lock
+sudo systemctl enable nfs-idmap
+sudo systemctl start rpcbind
+sudo systemctl start nfs-server
+sudo systemctl start nfs-lock
+sudo systemctl start nfs-idmap
