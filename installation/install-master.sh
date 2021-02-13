1d00de1617dd78b43bdc4a7742c46ca2780e5185
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2021-02-13 07:27:00 +0700

    master edit

--- installation/install-master.sh
+++ installation/install-master.sh
@@ -39,6 +39,6 @@ EOF
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo yum install -y kubelet-1.19.1 kubeadm-1.19.1 kubectl-1.19.1 --disableexcludes=kubernetes
 
 sudo systemctl enable --now kubelet
