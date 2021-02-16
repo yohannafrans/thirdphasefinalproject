20a25f34b2449777a74a9d53265e22f89654bb41
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2021-02-16 07:03:16 +0700

    cilium and calico

---
+++ installation/06-calico-master-bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubeadm init --kubernetes-version v1.19.1 --control-plane-endpoint "172.20.1.124:6443" --upload-certs --pod-network-cidr=172.20.2.0/24
+
+mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/kmdrn7/mykloud-kubernetes/master/yaml/cni/calico.yaml
